95a934bcde03820d48189b8d52772a6a488630c8
Jesus Antonio Moreno <73539839+JesusMorenoDur@users.noreply.github.com>
2024-03-11 09:09:05 -0700

    cambios

--- src/components/Layout/CustomBreadcrumb.tsx
+++ src/components/Layout/CustomBreadcrumb.tsx
@@ -8,72 +8,75 @@ import { useLocation } from "react-router-dom";
 import { Box, Container } from "@mui/material";
 
 function handleClick(event: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-  event.preventDefault();
-  console.info("You clicked a breadcrumb.");
+	event.preventDefault();
+	console.info("You clicked a breadcrumb.");
 }
 
 const messagesByLink: Record<string, string> = {
-  "/": "Inicio",
-  "/compras/solicitud-compras": "Solicitud de compras",
-  "/compras/solicitud-compras/productos-solicitados-orden-compra":
-    "Solcitudes en Proceso",
-  "/compras/solicitud-compras/productos-stock-bajo": "Alerta de Productos",
-  "/compras/categorias/categoria": "Categoría",
-  "/compras/categorias/subcategoria": "Sub categoría",
-  "/compras/articulos/articulo": "Catálogo de Productos",
-  "/compras/articulos/articulo-existente": "Productos en Existencia",
-  "/compras/almacen": "Almacén",
-  "/compras/autorizacion-compras": "Autorización de ordenes de compra",
-  "/compras/proveedores": "Proveedores",
-  "/compras/configuracion-compras": "Configuración de compras",
+	"/": "Inicio",
+	"/compras/solicitud-compras": "Solicitud de compras",
+	"/compras/solicitud-compras/productos-solicitados-orden-compra":
+		"Solcitudes en Proceso",
+	"/compras/solicitud-compras/productos-stock-bajo": "Alerta de Productos",
+	"/compras/categorias/categoria": "Categoría",
+	"/compras/categorias/subcategoria": "Sub categoría",
+	"/compras/articulos/articulo": "Catálogo de Productos",
+	"/compras/articulos/articulo-existente": "Productos en Existencia",
+	"/compras/almacen": "Almacén",
+	"/compras/autorizacion-compras": "Autorización de ordenes de compra",
+	"/compras/proveedores": "Proveedores",
+	"/compras/configuracion-compras": "Configuración de compras",
+	"/compras/autorizacion-compras/autorizaciones": "Autorizaciones",
+	"/compras/autorizacion-compras/historial-autorizaciones":
+		"Historial de Autorizaciones",
 };
 
 const CustomBreadcrumb = () => {
-  const location = useLocation();
-  const [currentPageMessage, setCurrentPageMessage] = useState<string>("");
+	const location = useLocation();
+	const [currentPageMessage, setCurrentPageMessage] = useState<string>("");
 
-  useEffect(() => {
-    const currentMessage = messagesByLink[location.pathname] || "";
-    setCurrentPageMessage(currentMessage);
-  }, [location.pathname]);
+	useEffect(() => {
+		const currentMessage = messagesByLink[location.pathname] || "";
+		setCurrentPageMessage(currentMessage);
+	}, [location.pathname]);
 
-  return (
-    <div role="presentation" onClick={handleClick}>
-      <Box component="main" sx={{ pt: 3, pl: 3, flexGrow: 1 }}>
-        <Container maxWidth={"xl"}>
-          <Breadcrumbs aria-label="breadcrumb">
-            <Typography
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                color: "black",
-                fontWeight: 600,
-              }}
-              color="inherit"
-            >
-              <HomeIcon sx={{ mr: 0.5, fontSize: "22px" }} fontSize="inherit" />
-              Inicio
-            </Typography>
-            <Typography
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                color: "black",
-                fontWeight: 600,
-              }}
-              color="text.primary"
-            >
-              <GrainIcon
-                sx={{ mr: 0.5, fontSize: "22px" }}
-                fontSize="inherit"
-              />
-              {currentPageMessage}
-            </Typography>
-          </Breadcrumbs>
-        </Container>
-      </Box>
-    </div>
-  );
+	return (
+		<div role="presentation" onClick={handleClick}>
+			<Box component="main" sx={{ pt: 3, pl: 3, flexGrow: 1 }}>
+				<Container maxWidth={"xl"}>
+					<Breadcrumbs aria-label="breadcrumb">
+						<Typography
+							sx={{
+								display: "flex",
+								alignItems: "center",
+								color: "black",
+								fontWeight: 600,
+							}}
+							color="inherit"
+						>
+							<HomeIcon sx={{ mr: 0.5, fontSize: "22px" }} fontSize="inherit" />
+							Inicio
+						</Typography>
+						<Typography
+							sx={{
+								display: "flex",
+								alignItems: "center",
+								color: "black",
+								fontWeight: 600,
+							}}
+							color="text.primary"
+						>
+							<GrainIcon
+								sx={{ mr: 0.5, fontSize: "22px" }}
+								fontSize="inherit"
+							/>
+							{currentPageMessage}
+						</Typography>
+					</Breadcrumbs>
+				</Container>
+			</Box>
+		</div>
+	);
 };
 
 export default CustomBreadcrumb;
