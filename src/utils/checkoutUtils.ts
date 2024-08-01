29cbfd18e23f2ca728316b9d0b90f7a17b27f029
Jesus Antonio Moreno <73539839+JesusMorenoDur@users.noreply.github.com>
2024-08-01 13:53:51 -0700

    Update checkoutUtils.ts

--- src/utils/checkoutUtils.ts
+++ src/utils/checkoutUtils.ts
@@ -18,4 +18,4 @@ export const hashEstatusToString: { [key: number]: string } = {
   0: 'Cancelado',
 };
 
-export const leaveConcepts = ['Consulta Sami', 'Radiografías', 'Farmacia', 'Hospitalización', 'Arco en C'];
+export const leaveConcepts = ['Consulta Sami', 'Radiografías', 'Farmacia', 'Hospitalización', 'Arco en C', 'Servicios'];
