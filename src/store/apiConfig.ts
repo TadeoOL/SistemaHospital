93a0c5c3399eb64092721db06765bf3998dd0d50
Tadeo O. <l19330633@hermosillo.tecnm.mx>
2024-12-04 12:23:53 -0700

    hotfix#Invoicing-apiKey

--- src/store/apiConfig.ts
+++ src/store/apiConfig.ts
@@ -1,31 +1,19 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface ApiConfigStore {
   apiUrl: string;
   setApiUrl: (url: string) => void;
 }
 
-const localStorageMiddleware = (config: any) => (set: any, get: any, api: any) => {
-  const initialState = config(
-    (args: any) => {
-      set(args);
-      localStorage.setItem('api-config-facturacion', args.apiUrl);
-    },
-    get,
-    api
-  );
-
-  const storedApiUrl = localStorage.getItem('api-config-facturacion');
-  if (storedApiUrl) {
-    set({ apiUrl: storedApiUrl });
-  }
-
-  return initialState;
-};
-
 export const useApiConfigStore = create<ApiConfigStore>()(
-  localStorageMiddleware((set: any) => ({
-    apiUrl: '',
-    setApiUrl: (url: any) => set({ apiUrl: url }),
-  }))
+  persist(
+    (set) => ({
+      apiUrl: '',
+      setApiUrl: (url) => set({ apiUrl: url }),
+    }),
+    {
+      name: 'api-config-facturacion',
+    }
+  )
 );
